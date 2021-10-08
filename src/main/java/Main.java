20981d931ca862211fddc450011ca9951fd9b45c
ivev <spechtator@gmail.com>
2021-10-08 14:00:21 +0200

    023 get simple sql-request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 //    private static final String USERNAME = "c##spechtator";
@@ -14,14 +12,29 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
         Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
 
         try {
             conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
-            System.out.println("Connected!");
+
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            rs = stmt.executeQuery("SELECT * FROM states");
+
+            rs.last();
+            System.out.println("Number of rows:" + rs.getRow());
+
         } catch (SQLException throwables) {
             throwables.printStackTrace();
+
         } finally {
-            if (conn != null){
+            if (rs != null){
+                rs.close();
+            }
+        if (stmt != null){
+                stmt.close();
+            }
+        if (conn != null){
                 conn.close();
             }
         }
