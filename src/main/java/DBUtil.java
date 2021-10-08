b13e0ee025a6a33f78930aee9ba8f6577f5ca987
ivev <spechtator@gmail.com>
2021-10-08 21:43:09 +0200

    031f multiconnect fix

--- src/main/java/DBUtil.java
+++ src/main/java/DBUtil.java
@@ -3,17 +3,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String USERNAME = "dbuser";
-    private static final String PASSWORD = "dbpassword";
+    private static final String M_USERNAME = "dbuser";
+    private static final String M_PASSWORD = "dbpassword";
     private static final String M_CONN_STRING ="jdbc:mysql://localhost/explorecalifornia";
+    private static final String O_USERNAME = "c##spechtator";
+    private static final String O_PASSWORD = "123";
     private static final String O_CONN_STRING ="jdbc:oracle:thin:@localhost:1521/orcl";
 
     public static Connection getConnection(DBType dbType) throws SQLException {
         switch (dbType) {
             case ORACLE:
-                return DriverManager.getConnection(O_CONN_STRING, USERNAME, PASSWORD);
+                return DriverManager.getConnection(O_CONN_STRING, O_USERNAME, O_PASSWORD);
             case MYSQL:
-                return DriverManager.getConnection(M_CONN_STRING, USERNAME, PASSWORD);
+                return DriverManager.getConnection(M_CONN_STRING, M_USERNAME, M_PASSWORD);
 
             default: return null;
         }
