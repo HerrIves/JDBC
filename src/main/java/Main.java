07e2fa8238e8f168acfef95dcb3dca1d5edffe89
ivev <spechtator@gmail.com>
2021-10-08 13:40:11 +0200

    03 connecting to mySQL db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
-    private static final String USERNAME = "c##spechtator";
-    private static final String PASSWORD = "123";
-    private static final String CONN_STRING =
-            "jdbc:oracle:thin:@localhost:1521/orcl";
+//    private static final String USERNAME = "c##spechtator";
+//    private static final String PASSWORD = "123";
+//    private static final String CONN_STRING = "jdbc:oracle:thin:@localhost:1521/orcl";
+
+    private static final String USERNAME = "dbuser";
+    private static final String PASSWORD = "dbpassword";
+    private static final String CONN_STRING ="jdbc:mysql://localhost/explorecalifornia";
 
     public static void main(String[] args) throws SQLException {
 
