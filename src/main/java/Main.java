e7516ab8f0155894e5394b464eaaf1301c71055a
ivev <spechtator@gmail.com>
2021-10-12 23:11:56 +0200

    033 try with resources

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,11 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
 
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-
-            conn = DBUtil.getConnection(
-/*                    DBType.MYSQL
-// */                 DBType.ORACLE
-            );
-            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            rs = stmt.executeQuery("SELECT * FROM package");
+        try (
+                Connection conn = DBUtil.getConnection(DBType.ORACLE);
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                ResultSet rs = stmt.executeQuery("SELECT * FROM packages");
+                ){
 
             rs.last();
             System.out.println("Number of rows:" + rs.getRow());
@@ -23,18 +16,6 @@ public class Main {
         } catch (SQLException e) {
             DBUtil.processException(e);
 
-        } finally {
-            if (rs != null){
-                rs.close();
-            }
-        if (stmt != null){
-                stmt.close();
-            }
-        if (conn != null){
-                conn.close();
-            }
         }
-
-
     }
 }
