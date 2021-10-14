4967ccb976a029940e8a5031edb2e045e14e2766
ivev <spechtator@gmail.com>
2021-10-14 18:10:55 +0200

    044 prepstmt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,40 @@
 import tables.States;
+import tables.Tours;
+import util.InputHelper;
 
 import java.sql.*;
 
 public class Main {
+    private static final String SQL =
+            "SELECT tourId, tourName, price FROM tours WHERE price <= ?";
 
     public static void main(String[] args) throws SQLException {
 
+        double maxPrice;
+        try{
+            maxPrice = InputHelper.getDoubleInput("Enter a maximum price: ");
+        } catch (NumberFormatException e) {
+            System.err.println("Error: invalid number");
+            return;
+        }
+
+        ResultSet rs = null;
         try (
                 Connection conn = DBUtil.getConnection(DBType.MYSQL);
-                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-                ResultSet rs = stmt.executeQuery("SELECT * " +
-                                                      "FROM states " +
-                                                      "LIMIT 5, 5");
+                PreparedStatement stmt = conn.prepareStatement(
+                        SQL,
+                        ResultSet.TYPE_SCROLL_INSENSITIVE,
+                        ResultSet.CONCUR_READ_ONLY);
                 ){
-
-            States.displayData(rs);
+            stmt.setDouble(1, maxPrice);
+            rs = stmt.executeQuery();
+            Tours.displayData(rs);
 
         } catch (SQLException e) {
             DBUtil.processException(e);
-
+        }
+        finally {
+            if (rs != null) rs.close();
         }
     }
 }
